d54f0bb691906217823181ae82712c93b5fe5d23
Oscar Davies <oscar@designlogica.co.uk>
2024-10-19 18:43:55 +0100

    Test Code

--- PT-LIb/Core.cs
+++ PT-LIb/Core.cs
@@ -1,10 +1,32 @@
-﻿namespace PT_LIb
+﻿using ComputeSharp;
+using ComputeSharp.Descriptors;
+using ComputeSharp.Interop;
+
+namespace PT_LIb
 {
+    [ThreadGroupSize(DefaultThreadGroupSizes.X)]
+    [GeneratedComputeShaderDescriptor]
+    public readonly partial struct MultiplyByTwo(ReadWriteBuffer<float> buffer) : IComputeShader, IComputeShaderDescriptor<MultiplyByTwo>
+    {
+        public void Execute()
+        {
+            buffer[ThreadIds.X] *= 2;
+        }
+    }
+
     public class Core
     {
         public static void Init()
         {
             Console.WriteLine("Hello, World!");
+
+            var numbers = new float[] { 1, 2, 3, 4, 5 };
+
+            using ReadWriteBuffer<float> buffer = GraphicsDevice.GetDefault().AllocateReadWriteBuffer(numbers);
+
+            GraphicsDevice.GetDefault().For(buffer.Length, new MultiplyByTwo(buffer));
+
+            buffer.CopyTo(numbers);
         }
     }
 }
