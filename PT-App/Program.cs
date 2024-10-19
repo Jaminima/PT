426b91302f9ff40d60516a7046326134ca6d4a46
Oscar Davies <oscar@designlogica.co.uk>
2024-10-19 18:00:50 +0100

    Add project files.

---
+++ PT-App/Program.cs
@@ -0,0 +1,13 @@
+﻿using PT_LIb;
+
+namespace PT_App
+{
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Core.Init();
+        }
+    }
+}
---
+++ PT-LIb/Core.cs
@@ -0,0 +1,10 @@
+﻿namespace PT_LIb
+{
+    public class Core
+    {
+        public static void Init()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
